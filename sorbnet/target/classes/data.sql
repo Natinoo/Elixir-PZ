d34fab5ee72250d9bd74cd72313b32a51657c661
CzeszQ <dawid.czeszek0@gmail.com>
2026-05-06 16:08:18 +0200

    Update Spring Boot 3.5.14, add .gitignore

--- sorbnet/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO bank_accounts (bank_id, bank_name, balance, debt_limit, blocked, overlimit_since, blocked_at)
-VALUES
-  ('NBP',   'Narodowy Bank Polski',  10000000, 0,       false, NULL, NULL),
-  ('PKO',   'PKO Bank Polski',        5000000, 2000000, false, NULL, NULL),
-  ('PEKAO', 'Bank Pekao',             4000000, 1500000, false, NULL, NULL),
-  ('MBANK', 'mBank',                  3000000, 1000000, false, NULL, NULL),
-  ('ING',   'ING Bank Śląski',        3500000, 1200000, false, NULL, NULL),
-  ('BNP',   'BNP Paribas',            2500000, 800000,  false, NULL, NULL)
-ON CONFLICT (bank_id) DO NOTHING;
